cfd4f92cffb52d45b742c7b041893264a5bfed41
Ajay Jangir <ajayjangir5252@gmail.com>
2025-01-12 19:06:58 +0530

    repo backup: autoUpdate true

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@
 ### Version 1.03 - Refactored
 
 $debug = $false
-$autoUpdate = $false
+$autoUpdate = $true
 
 if ($debug) {
     Write-Host "#######################################" -ForegroundColor Red
